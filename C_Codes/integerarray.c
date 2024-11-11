33f16f8c7c1c91011fa9c1f905974ded510a5169
randomtrydude <randomtrydude@protonmail.com>
2024-11-11 14:56:01 +0100

    assignment 10

---
+++ C_Codes/integerarray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "colors.h"
+
+
+int main(void){
+	int integers[] = {0,1,2,3,4,5,6,7,8,9};
+
+	printf("The array has %i elements.",sizeof(integers)/4);
+	return 0;
+	
+
+}

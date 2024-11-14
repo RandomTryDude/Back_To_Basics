eebcc31ca353d09e93aee791d339efb5b794d19d
randomtrydude <randomtrydude@protonmail.com>
2024-11-14 17:17:22 +0100

    Assignment-16

---
+++ C_Codes/Assignment-16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "colors.h"
+
+
+
+
+int main(void){
+
+	int array[10] = {1,2,3,4,5,6,7,8,9,0};
+
+	printf("%d\n",array); // Without the index print the value it ptr to
+	
+	printf("%d\n",array[0]);
+
+
+	return 0;
+}

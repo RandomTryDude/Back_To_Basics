39a662e1094d197ac6832197093c5fad924cc9ef
randomtrydude <randomtrydude@protonmail.com>
2024-11-14 17:22:44 +0100

    Assignment-17

---
+++ C_Codes/assignement-17.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include "colors.h"
+
+
+int printer(void); // declare the function prototype so compiler != angry 
+
+
+int main(void){
+
+	printer();
+
+return 0;
+}
+
+int printer(void){
+
+
+	printf("Hello,World!\n");
+	return 0;
+}
